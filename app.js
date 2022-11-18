bdde79e0db2290fde910e6aad0a8950c74affc97
DanielL <rra.daniel@gmail.com>
2022-11-17 22:07:42 -0300

    first commit

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
 require('dotenv').config();
 const express= require('express');
 const hbs = require('hbs');
-
 const app = express();
 const port = process.env.PORT;
 
-
 // Handlebars
+
 app.set('view engine', 'hbs');
+
 hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 
 
 
 //Servir contenido estatico
+app.use("/public", express.static('public')); 
 app.use(express.static('public'));
+
 
 app.get('/',(req,res)=>{
     res.render('home',{
-        nombre:'Daniel Rosas',
-        titulo: 'Curso'
+        titulo: 'DogIn'
     });
 });
 
-app.get('/generic',(req,res)=>{
-    res.render('generic',{
-        nombre:'Daniel Rosas',
-        titulo: 'Generic'
+
+app.get('/busqueda',(req,res)=>{
+    res.render('busqueda',{
+        titulo: 'Dogin'
     });
 });
 
-app.get('/elements',(req,res)=>{
-    res.render('elements',{
-        nombre:'Daniel Rosas',
-        titulo: 'Generic'
+app.get('/registro-cuidador',(req,res)=>{
+    res.render('registro-cuidador',{
+        titulo: 'Dogin'
+    });
+});
+app.get('/registro',(req,res)=>{
+    res.render('registro',{
+        titulo: 'Dogin'
     });
 });
 
@@ -40,6 +45,7 @@ app.get('/elements',(req,res)=>{
 app.get('*',(req,res)=>{
     res.sendFile(__dirname + '/public/404.html');
 });
+
 
 
 app.listen(port, ()=>{
